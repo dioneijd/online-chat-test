c8a6b85010f91825d7fa7b012c4a71ec20959118
Dionei J. Delangolo <dionei.jd@gmail.com>
2020-04-14 19:02:47 -0300

    deploy heroku 3

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+require('dotenv').config()
+   
+
 const express = require('express')
 const path = require('path')
 const routes = require('./routes')
@@ -7,17 +10,13 @@ const app = express();
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
-// app.use(express.static(path.join(__dirname, 'public')))
-// app.set('views', path.join(__dirname, 'public'))
-// app.engine('html', require('ejs').renderFile)
-// app.set('view engine', 'html')
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'public'))
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
 
 
 app.use(routes)
-
-// app.use('/', (req, res) => {
-//     res.render('index.html')
-// })
 
 
 let messages = []
